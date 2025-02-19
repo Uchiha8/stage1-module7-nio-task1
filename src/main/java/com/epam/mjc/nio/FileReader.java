73491e7f91c91a9009590ea7c7bf3efb79f96e47
Uchiha@8 <2110107@newuu.uz>
2025-02-19 20:54:05 +0500

    Implement file parsing logic in FileReader class
    Added functionality to parse a provided file and extract profile data into a Profile object. Introduced error handling and updated the method to properly read and process file content.

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String pathFile = file.getPath();
+        Map<String, String> profileData = new HashMap<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(pathFile))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] data = line.split(": ");
+                profileData.put(data[0].trim(), data[1].trim());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(
+                profileData.get("Name"),
+                Integer.parseInt(profileData.get("Age")),
+                profileData.get("Email"),
+                Long.parseLong(profileData.get("Phone"))
+        );
     }
 }
